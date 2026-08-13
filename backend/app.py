c8d3cf2697988654f0b21ce73de7e56c2c6352fd
Anna <1695442890@qq.com>
2026-08-13 12:24:41 +0800

    test: 验证自动触发03

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello CI/CD!"
+    return "Hello CI/CD001!"
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=5000)
